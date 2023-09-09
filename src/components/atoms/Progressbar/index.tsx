dd279fbea18bf3d24775f03c742054c5eb24f667
zieun <jieunpark247@gmail.com>
2023-09-09 19:10:29 +0900

    [박지은] 스크롤애니메이션 동작 이슈 수정 완료

--- src/components/atoms/Progressbar/index.tsx
+++ src/components/atoms/Progressbar/index.tsx
@@ -1,15 +1,29 @@
 import { styled } from 'styled-components';
-
 import { TOPBAR_HEIGHT } from '@/constants/theme';
+import { useEffect, useRef, useState } from 'react';
 
 function Progressbar() {
+  const timeline = useRef();
+  const [scrollPercent, setScrollPercent] = useState(-100);
+  const setTimeline = () => {
+    const percent = Math.floor(
+      (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100
+    );
+    setScrollPercent(-(100 - percent));
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', setTimeline);
+    return () => {
+      window.removeEventListener('scroll', setTimeline);
+    };
+  }, []);
+
   return (
     <Wrap>
-      <ProgressbarStyled />
+      <ProgressbarStyled ref={timeline} scrollPercent={scrollPercent} />
     </Wrap>
   );
 }
-
 const Wrap = styled.div`
   position: fixed;
   top: ${TOPBAR_HEIGHT}px;
@@ -21,22 +35,11 @@ const Wrap = styled.div`
 `;
 
 const ProgressbarStyled = styled.div`
-  animation: grow-progress auto linear forwards;
-  animation-timeline: scroll(block root);
+  transform: translateX(${(props) => props.scrollPercent}%);
+  transition: 0.1s all ease;
   background: red;
   width: 100%;
   height: 10px;
-  transform-origin: 0 50%;
-  transform: scaleX(0);
-
-  @keyframes grow-progress {
-    from {
-      transform: scaleX(0);
-    }
-    to {
-      transform: scaleX(1);
-    }
-  }
 `;
 
 export default Progressbar;
